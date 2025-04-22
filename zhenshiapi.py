c2c5b1172647d64dbf20a6a3d5c5a36094ca6177
Yutsing Kuh <yutsing@outlook.com>
2025-04-22 16:21:23 +0800

    Add some functions; Add proxy support; Add .gitignore

--- zhenshiapi.py
+++ zhenshiapi.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import hashlib
 
 default_headers = {
     "Host": "www.xionger.icu:8080",
@@ -9,17 +10,50 @@ default_headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
+def username_to_password(username):
+        # 固定的盐值
+        tail = "SomeThingAddToUsername7527"
+        
+        # 拼接用户名和盐值
+        salted_username = username + tail
+        
+        # 使用SHA-256哈希算法
+        sha256 = hashlib.sha256()
+        sha256.update(salted_username.encode('utf-8'))
+        
+        # 将哈希值转换为十六进制字符串
+        hashed_password = sha256.hexdigest()
+        
+        return hashed_password
+
+def login(username, picurl, nickname, deviceid, ip):
+    url = f"http://{default_headers['Host']}/user/login"
+    password = username_to_password(username)
+    payload = f"username={username}&password={password}&picurl={picurl}&nickname={nickname}&deviceid={deviceid}&ip={ip}"
+    headers = default_headers.copy()
+    headers["Content-Length"] = str(len(payload))
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+        response.raise_for_status()  # Raise an error for bad responses
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
 class Zhenshi:
-    def __init__(self, account, ):
-        self.Host = "http://www.xionger.icu:8080"
-        self.default_headers = default_headers
-        self.default_headers["Authorization"] = account
+    def __init__(self, auth = None, proxy = None):
+        if auth is None:
+            raise ValueError("Account must be provided")
+        self.Host = f"http://{default_headers['Host']}"
+        self.default_headers = default_headers.copy()
+        self.default_headers["Authorization"] = auth
+        self.proxy = proxy
     
     def send_request(self, endpoint, headers, data):
         submit_headers = headers.copy()  # Create a copy to avoid shared state issues
         submit_headers["Content-Length"] = str(len(data))
         try:
-            response = requests.post(f"{self.Host}/{endpoint}", headers=submit_headers, data=data)
+            response = requests.post(f"{self.Host}/{endpoint}", headers=submit_headers, data=data, proxies=self.proxy)
             response.raise_for_status()  # Raise an error for bad responses
             return response.json()
         except requests.RequestException as e:
@@ -28,6 +62,12 @@ class Zhenshi:
         except json.JSONDecodeError as e:
             print(f"JSON decode error: {e}")
             return None
+        
+    def check(self):
+        endpoint = "user/checked"
+        data = ""
+        response = self.send_request(endpoint, self.default_headers, data)
+        return response
         
     def get_userinfo(self):
         endpoint = "user/userInfo"
@@ -66,6 +106,12 @@ class Zhenshi:
         response = self.send_request(endpoint, self.default_headers, data)
         return response
     
+    def cancelorder(self, id):
+        endpoint = "trade/cancelOrder"
+        data = f"id={id}"
+        response = self.send_request(endpoint, self.default_headers, data)
+        return response
+    
     def add_lotteryorder(self, number, multiple):
         endpoint = "lottery/addLotteryOrder"
         data = f"number={number}&multiple={multiple}"
@@ -83,3 +129,26 @@ class Zhenshi:
         data = f"id={id}"
         response = self.send_request(endpoint, self.default_headers, data)
         return response
+    
+    def reward(self):
+        endpoint = "user/reward"
+        data = ""
+        response = self.send_request(endpoint, self.default_headers, data)
+        return response
+    
+    def update_nickname(self, nickname):
+        endpoint = "user/updateNickname"
+        data = f"nickname={nickname}"
+        response = self.send_request(endpoint, self.default_headers, data)
+        return response
+    
+    def get_rank(self):
+        endpoint = "user/getRank"
+        data = ""
+        response = self.send_request(endpoint, self.default_headers, data)
+        return response
+
+    def cancel_all_orders(self):
+        orders = self.get_allorder()['data']
+        for o in orders:
+            self.cancelorder(o['id'])
